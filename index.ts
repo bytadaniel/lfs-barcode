5dffc4daa8e87ed71265483e65358d1be45efdd9
Daniel <danielbyta.work@gmail.com>
2022-11-03 14:17:48 +0200

    add utils

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { StickerCanvasElement  } from './src/api/StickerCanvasElement'
 import { PaperA4ElementManager } from './src/api/PaperA4ElementManager'
 import { PDFImageReader } from './src/api/PDFImageReader'
+import * as utils from './src/core/utils/common'
 
 export {
 	StickerCanvasElement,
 	PaperA4ElementManager,
-	PDFImageReader
+	PDFImageReader,
+	utils
 }
