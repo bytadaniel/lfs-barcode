96214e94c80a6b07e4bc9e4066e795153124d942
Daniel <danielbyta.work@gmail.com>
2022-11-06 18:17:22 +0200

    dev

--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -12,7 +12,7 @@ export function createSvgFromBarcode(barcode: string, type: BarcodeType) {
 	const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
 	JsBarcode(svgNode, barcode, {
-		font: "OCR-B",
+		font: "Arial",
 		displayValue: true,
 		xmlDocument: document,
 		format: type,
@@ -23,9 +23,9 @@ export function createSvgFromBarcode(barcode: string, type: BarcodeType) {
 	return svg
 }
 
-export async function createBarcodeImage(_type: BarcodeType, data: string, width: number, height: number) {
+export async function createBarcodeImage(type: BarcodeType, data: string, width: number, height: number) {
 	return new Promise<any>(async (resolve) => {
-		// const stringSvgHTMLElement = createSvgFromBarcode(data, type)
+		const stringSvgHTMLElement = createSvgFromBarcode(data, type)
 
 		const barcodeCanvas = getBarcode({
 			...defaultOptions,
@@ -38,15 +38,15 @@ export async function createBarcodeImage(_type: BarcodeType, data: string, width
 
 		console.log(barcodeCanvas.toDataURL())
 
-		const buffer = await sharp(barcodeCanvas.toBuffer())
-			.resize(width, height, { fit: 'fill' })
-			.png({ quality: 100 })
-			.toBuffer()
-
-		// const buffer = await sharp(Buffer.from(stringSvgHTMLElement))
+		// const buffer = await sharp(barcodeCanvas.toBuffer())
 		// 	.resize(width, height, { fit: 'fill' })
 		// 	.png({ quality: 100 })
 		// 	.toBuffer()
+
+		const buffer = await sharp(Buffer.from(stringSvgHTMLElement))
+			.resize(width, height, { fit: 'fill' })
+			.png({ quality: 100 })
+			.toBuffer()
 
 		const image = await loadImage(buffer)
 
