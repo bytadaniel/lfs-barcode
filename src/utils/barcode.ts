0e1600f19a10ce8200a0ef52489fce94c9e7683e
Daniel <danielbyta.work@gmail.com>
2022-11-06 12:37:37 +0200

    reder font in barcode

--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -8,12 +8,14 @@ export function createSvgFromBarcode(barcode: string, type: BarcodeType) {
 	const xmlSerializer = new XMLSerializer();
 	const document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null);
 	const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-	
-	JsBarcode(svgNode, barcode, {
-		xmlDocument: document,
-		format: type,
-		displayValue: true,
-	})
+
+	JsBarcode(svgNode)
+		.options({
+			font: "OCR-B",
+			xmlDocument: document,
+			format: type,
+		})
+		.render()
 	
 	const svg = xmlSerializer.serializeToString(svgNode);
 	return svg
