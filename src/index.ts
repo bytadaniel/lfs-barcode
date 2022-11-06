3c79e73deea79befb51a0c5321934ac99609402c
Daniel <danielbyta.work@gmail.com>
2022-11-06 11:28:17 +0200

    try

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { StickerCanvasElement  } from './api/StickerCanvasElement'
+import { PaperA4ElementManager } from './api/PaperA4ElementManager'
+import { PDFImageReader } from './api/PDFImageReader'
+import * as utils from './utils/common'
+
+import { registerFonts } from './utils/canvas'
+
+registerFonts()
+
+export {
+	StickerCanvasElement,
+	PaperA4ElementManager,
+	PDFImageReader,
+	utils
+}
